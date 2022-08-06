d95dfa323f630c165e542a3218af8e0a4efb8dc9
singpay <singpaynow@gmail.com.com>
2022-08-06 19:02:21 +0700

    begin

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# Solidity Framework Test
+In this repo we are testing Truffle, Hardhat and Foundry.
+
+You can run each of those with the respective commands in the sub directories. Do not forget to run npm install in truffle or hardhat before starting!
---
+++ hardhat/scripts/deployTokens-script.js
@@ -0,0 +1,36 @@
+async function main() {
+    const SampleToken = await ethers.getContractFactory("SampleToken");
+    const sampleToken = await SampleToken.deploy("Sample Token", "TOK", (50 * 10e18).toString());
+    await sampleToken.deployed();
+    
+    
+    const StakingToken = await ethers.getContractFactory("StakingToken");
+    const stakingToken = await StakingToken.deploy("Staking Token", "STA", sampleToken.address);
+    await stakingToken.deployed();
+    
+    const grantBurnTx = await sampleToken.grantRole(await sampleToken.BURNERROLE(), stakingToken.address);
+    await grantBurnTx.wait();
+    
+    
+    const grantMintTx = await sampleToken.grantRole(await sampleToken.MINTERROLE(), stakingToken.address);
+    await grantMintTx.wait();
+    
+    const stakeFiftyTx = await stakingToken.stake((5*10e18).toString());
+    
+    // // wait until the transaction is mined
+    await stakeFiftyTx.wait();
+    
+    
+    console.log("StakingToken deployed to:", stakingToken.address);
+    console.log("SampleToken deployed to:", sampleToken.address);
+    }
+    
+    // We recommend this pattern to be able to use async/await everywhere
+    // and properly handle errors.
+    main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
+    
